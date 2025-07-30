704ac4121c0f5dd5b0fb2c992a95e493e123d4e9
Alexey <superant@tut.by>
2025-07-30 16:12:22 +0300

    Тест по скорости List Dictionary для триггеров

--- src/Mvk2/Program.cs
+++ src/Mvk2/Program.cs
@@ -1,33 +1,100 @@
-﻿using WinGL;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Vge.Entity.Animation;
+using WinGL;
+using WinGL.Util;
 
 namespace Mvk2
 {
     class Program
     {
-        
+        //static Dictionary<string, ModelAnimationClip> ddD = new Dictionary<string, ModelAnimationClip>();
+        //static List<ModelAnimationClip> ddL = new List<ModelAnimationClip>();
+
+        //static void TestInit()
+        //{
+        //    for (int i = 0; i < 32; i++)
+        //    {
+        //        ModelAnimationClip model = new ModelAnimationClip(i.ToString(), Vge.Entity.Model.ModelLoop.Loop, 0, 1, 1, new BoneAnimationChannel[] { });
+        //        ddD.Add("123456789" + i.ToString(), model);
+        //        ddL.Add(model);
+        //    }
+        //}
+
+        //static double TestD(string s)
+        //{
+        //    Stopwatch stopwatch = new Stopwatch();
+        //    stopwatch.Start();
+        //    ModelAnimationClip model;
+        //    for (int J = 0; J < 10000; J++)
+        //    { 
+        //        if (ddD.ContainsKey(s)) 
+        //            model = ddD[s];
+        //    }
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"D: {stopwatch.Elapsed.TotalMilliseconds}");
+        //    return stopwatch.Elapsed.TotalMilliseconds;
+        //}
+
+        //static double TestL(int i)
+        //{
+        //    Stopwatch stopwatch = new Stopwatch();
+        //    stopwatch.Start();
+        //    ModelAnimationClip model;
+        //    for (int J = 0; J < 10000; J++)
+        //    { 
+        //        if (ddL.Count > i) model = ddL[i];
+        //    }
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"L: {stopwatch.Elapsed.TotalMilliseconds}");
+        //    return stopwatch.Elapsed.TotalMilliseconds;
+        //}
+
         static void Main(string[] args)
         {
+
             //Glm.Initialized();
             //Vector3 v = new Vector3(1, 0, 0);
             //Vector3 v2 = new Vector3(1, 0, 0);
-            //for (int i = -180; i < 360; i+=5)
+            //for (int i = -180; i < 360; i += 5)
             //{
             //    v2 = Glm.Rotate(new Vector3(1, 0, 0), Glm.Radians(i), new Vector3(0, 1, 0));
             //    Console.WriteLine(i + " = " + Glm.Dot(v, v2));
             //}
             //Console.Read();
-            
-            
+
+
 
             Window.Run(new WindowMvk());
 
+            //TestInit();
             //double t = 0;
-            //for (int i = 0; i < 25; i++)
+            //for (int i = 0; i < 5; i++)
             //{
-            //    t += Test();
+            //    t += TestD("1234567895");
             //}
-            //Console.WriteLine($" ==: {t}");
+            //Console.WriteLine($"5 ==: {t}");
+            //t = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    t += TestD("12345678920"); 
+            //}
+            //Console.WriteLine($"20 ==: {t}");
+            //t = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    t += TestL(5);
+            //}
+            //Console.WriteLine($" 5==: {t}");
+            //t = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    t += TestL(20);
+            //}
+            //Console.WriteLine($" 20==: {t}");
             //Console.Read();
+
         }
 
         //private static double Test()
