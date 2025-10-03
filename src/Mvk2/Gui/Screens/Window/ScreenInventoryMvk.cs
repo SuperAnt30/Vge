e43acb601c824eb5cce84bf42831748754eed71c
Alexey <53894317+SuperAnt30@users.noreply.github.com>
2025-10-03 17:01:12 +0300

    Окно хранилища

--- src/Mvk2/Gui/Screens/Window/ScreenInventoryMvk.cs
+++ src/Mvk2/Gui/Screens/Window/ScreenInventoryMvk.cs
@@ -1,16 +1,7 @@
-﻿using Mvk2.Entity.List;
-using Mvk2.Games;
-using Mvk2.Gui.Controls;
-using Mvk2.Packets;
-using Mvk2.Renderer;
+﻿using Mvk2.Packets;
 using System;
-using Vge.Entity.Inventory;
 using Vge.Entity.Render;
-using Vge.Gui.Controls;
-using Vge.Gui.Screens;
-using Vge.Item;
 using Vge.Network.Packets.Client;
-using Vge.Renderer.Font;
 using WinGL.Actions;
 using WinGL.Util;
 
@@ -19,54 +10,24 @@ namespace Mvk2.Gui.Screens
     /// <summary>
     /// Окно инвентаря для игры Малювеки 2
     /// </summary>
-    public class ScreenInventoryMvk : ScreenWindow
+    public class ScreenInventoryMvk : ScreenStorageMvk
     {
-        private readonly WindowMvk _windowMvk;
-
-        protected readonly RenderMvk _render;
-
-        /// <summary>
-        /// Игрок мода, на клиенте
-        /// </summary>
-        private readonly PlayerClientOwnerMvk _player;
-
-        private readonly ControlSlot[] _slot;
-
-        /// <summary>
-        /// Иконка для предмета в воздухе
-        /// </summary>
-        private readonly ControlIcon _icon;
-        /// <summary>
-        /// Стак который используется в перемещении из слотов, образно он в указателе мыши
-        /// </summary>
-        private ItemStack _stakAir;
-
         public ScreenInventoryMvk(WindowMvk window) : base(window, 512, 354)
         {
-            _windowMvk = window;
-            _render = _windowMvk.GetRender();
-            _toolTip = new ToolTipMvk(window);
-
-            _slot = new ControlSlot[19 + 25];
-
-            _icon = new ControlIcon(_windowMvk, null);
-
-            ControlSlot slot;
-            for (int i = 0; i < _slot.Length; i++)
-            {
-                slot = new ControlSlot(window, (byte)i,
-                    _windowMvk.Game.Player.Inventory.GetStackInSlot(i));
-                slot.ClickLeft += (sender, e) => _SendPacket(((ControlSlot)sender).SlotId, false);
-                slot.ClickRight += (sender, e) => _SendPacket(((ControlSlot)sender).SlotId, true);
-                _slot[i] = slot;
-            }
-            _player = ((GameModClientMvk)_windowMvk.Game.ModClient).Player;
-            _player.InvPlayer.SlotSetted += InvPlayer_SlotSetted;
-            _player.InvPlayer.LimitBackpackChanged += InvPlayer_LimitBackpackChanged;
+            _player.InvPlayer.LimitBackpackChanged += _InvPlayer_LimitBackpackChanged;
 
             _windowMvk.Game.TrancivePacket(new PacketC0EClickWindow((byte)EnumActionClickWindow.OpenInventory));
             _UpBackpackEnabled();
         }
+
+        /// <summary>
+        /// Количество слотов
+        /// </summary>
+        protected override int _GetSlotCount() => 19 + 25;
+        /// <summary>
+        /// Название заголовка
+        /// </summary>
+        protected override string _GetTitle() => L.T("Inventory");
 
         /// <summary>
         /// Обновить слоты рюкзака
@@ -87,61 +48,8 @@ namespace Mvk2.Gui.Screens
         /// <summary>
         /// Событие изменён лимит рюкзака
         /// </summary>
-        private void InvPlayer_LimitBackpackChanged(object sender, EventArgs e)
+        private void _InvPlayer_LimitBackpackChanged(object sender, EventArgs e)
             => _UpBackpackEnabled();
-
-        /// <summary>
-        /// Изменился слот игрока
-        /// </summary>
-        private void InvPlayer_SlotSetted(object sender, SlotEventArgs e)
-        {
-            if (e.SlotId == 255)
-            {
-                _stakAir = e.Stack;
-                _icon.SetStack(e.Stack);
-            }
-            else
-            {
-                if (e.SlotId < _slot.Length)
-                {
-                    _slot[e.SlotId].SetStack(e.Stack);
-                }
-            }
-        }
-
-        private void _SendPacket(byte slotId, bool isRight)
-        {
-            // Нужна проверка, ненадо отправлять в пустую ячейку если в воздухе нет предмета
-            if (_player.Inventory.GetStackInSlot(slotId) != null || _stakAir != null)
-            {
-                _windowMvk.Game.TrancivePacket(new PacketC0EClickWindow(
-                    (byte)EnumActionClickWindow.ClickSlot,
-                    _windowMvk.Game.Key.KeyShift,
-                    isRight,
-                    slotId));
-            }
-        }
-
-        protected override void _InitTitle()
-        {
-            FontBase font = window.Render.FontMain;
-            _labelTitle = new Label(window, font, 250, 50, L.T("Inventory"));
-            _labelTitle.SetTextAlight(EnumAlight.Left, EnumAlightVert.Top);
-            _buttonCancel = new Button(window, font, 50, "X");
-        }
-
-        /// <summary>
-        /// Запускается при создании объекта и при смене режима FullScreen
-        /// </summary>
-        protected override void OnInitialize()
-        {
-            base.OnInitialize();
-            for (int i = 0; i < _slot.Length; i++)
-            {
-                AddControls(_slot[i]);
-            }
-            AddControls(_icon);
-        }
 
         /// <summary>
         /// Изменён размер окна
@@ -190,29 +98,6 @@ namespace Mvk2.Gui.Screens
         }
 
         /// <summary>
-        /// Клик за пределами окна
-        /// </summary>
-        protected override void _OnClickOutsideWindow() => _ThrowTheSlot();
-
-        /// <summary>
-        /// Выбросить слот который в руке
-        /// </summary>
-        protected virtual void _ThrowTheSlot()
-        {
-            if (_stakAir != null)
-            {
-                _windowMvk.Game.TrancivePacket(
-                    new PacketC0EClickWindow((byte)EnumActionClickWindow.ThrowOutAir));
-            }
-        }
-
-        /// <summary>
-        /// Происходит перед закрытием окна
-        /// </summary>
-        protected virtual void _OnFinishing() => _windowMvk.Game.TrancivePacket(
-            new PacketC0EClickWindow((byte)EnumActionClickWindow.Close));
-        
-        /// <summary>
         /// Запустить текстуру фона
         /// </summary>
         protected override void _BindTextureBg() => _windowMvk.GetRender().BindTextureInventory();
@@ -243,12 +128,6 @@ namespace Mvk2.Gui.Screens
             }
 
             _toolTip.Draw();
-        }
-
-        public override void Dispose()
-        {
-            base.Dispose();
-            _OnFinishing();
         }
     }
 }
---
+++ src/Mvk2/Gui/Screens/Window/ScreenStorageMvk.cs
@@ -0,0 +1,191 @@
+﻿using Mvk2.Entity.List;
+using Mvk2.Games;
+using Mvk2.Gui.Controls;
+using Mvk2.Packets;
+using Mvk2.Renderer;
+using Vge.Entity.Inventory;
+using Vge.Gui.Controls;
+using Vge.Gui.Screens;
+using Vge.Item;
+using Vge.Network.Packets.Client;
+using Vge.Renderer.Font;
+
+namespace Mvk2.Gui.Screens
+{
+    /// <summary>
+    /// Окно хранилища для игры Малювеки 2
+    /// </summary>
+    public abstract class ScreenStorageMvk : ScreenWindow
+    {
+        protected readonly WindowMvk _windowMvk;
+
+        protected readonly RenderMvk _render;
+
+        /// <summary>
+        /// Игрок мода, на клиенте
+        /// </summary>
+        protected readonly PlayerClientOwnerMvk _player;
+
+        protected readonly ControlSlot[] _slot;
+
+        /// <summary>
+        /// Иконка для предмета в воздухе
+        /// </summary>
+        protected readonly ControlIcon _icon;
+        /// <summary>
+        /// Стак который используется в перемещении из слотов, образно он в указателе мыши
+        /// </summary>
+        protected ItemStack _stakAir;
+
+        public ScreenStorageMvk(WindowMvk window, int width, int height) : base(window, width, height)
+        {
+            _windowMvk = window;
+            _render = _windowMvk.GetRender();
+            _toolTip = new ToolTipMvk(window);
+
+            _slot = new ControlSlot[_GetSlotCount()];
+
+            _icon = new ControlIcon(_windowMvk, null);
+
+            ControlSlot slot;
+            for (int i = 0; i < _slot.Length; i++)
+            {
+                slot = new ControlSlot(window, (byte)i,
+                    _windowMvk.Game.Player.Inventory.GetStackInSlot(i));
+                slot.ClickLeft += (sender, e) => _SendPacket(((ControlSlot)sender).SlotId, false);
+                slot.ClickRight += (sender, e) => _SendPacket(((ControlSlot)sender).SlotId, true);
+                _slot[i] = slot;
+            }
+            _player = ((GameModClientMvk)_windowMvk.Game.ModClient).Player;
+            _player.InvPlayer.SlotSetted += _InvPlayer_SlotSetted;
+        }
+
+        /// <summary>
+        /// Количество слотов
+        /// </summary>
+        protected virtual int _GetSlotCount() => 0;
+
+        /// <summary>
+        /// Название заголовка
+        /// </summary>
+        protected virtual string _GetTitle() => "Storage";
+
+        /// <summary>
+        /// Изменился слот игрока
+        /// </summary>
+        private void _InvPlayer_SlotSetted(object sender, SlotEventArgs e)
+        {
+            if (e.SlotId == 255)
+            {
+                _stakAir = e.Stack;
+                _icon.SetStack(e.Stack);
+            }
+            else
+            {
+                if (e.SlotId < _slot.Length)
+                {
+                    _slot[e.SlotId].SetStack(e.Stack);
+                }
+            }
+        }
+
+        protected void _SendPacket(byte slotId, bool isRight)
+        {
+            // Нужна проверка, ненадо отправлять в пустую ячейку если в воздухе нет предмета
+            if (_player.Inventory.GetStackInSlot(slotId) != null || _stakAir != null)
+            {
+                _windowMvk.Game.TrancivePacket(new PacketC0EClickWindow(
+                    (byte)EnumActionClickWindow.ClickSlot,
+                    _windowMvk.Game.Key.KeyShift,
+                    isRight,
+                    slotId));
+            }
+        }
+
+        protected override void _InitTitle()
+        {
+            FontBase font = window.Render.FontMain;
+            _labelTitle = new Label(window, font, 250, 50, _GetTitle());
+            _labelTitle.SetTextAlight(EnumAlight.Left, EnumAlightVert.Top);
+            _buttonCancel = new Button(window, font, 50, "X");
+        }
+
+        /// <summary>
+        /// Запускается при создании объекта и при смене режима FullScreen
+        /// </summary>
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            for (int i = 0; i < _slot.Length; i++)
+            {
+                AddControls(_slot[i]);
+            }
+            AddControls(_icon);
+        }
+
+        /// <summary>
+        /// Изменён размер окна
+        /// </summary>
+        protected override void OnResized()
+        {
+            // Расположение окна
+            PosX = (Width - WidthWindow) / 2;
+            PosY = (Height - HeightWindow) / 2;
+            //PosY = 10;
+            //PosX = 10;
+            
+            base.OnResized();
+            _labelTitle.SetPosition(PosX + 16, PosY + 10);
+            _buttonCancel.SetPosition(PosX + WidthWindow - 50, PosY);
+
+            for (int i = 0; i < 8; i++)
+            {
+                _slot[i].SetPosition(PosX + 106 + i * 50, PosY + 300);
+            }
+            _slot[8].SetPosition(PosX + 6, PosY + 300);
+
+            for (int i = 0; i < 5; i++)
+            {
+                // Одежда
+                _slot[i + 9].SetPosition(PosX + 6, PosY + 36 + i * 50);
+                _slot[i + 14].SetPosition(PosX + 176, PosY + 36 + i * 50);
+
+                // Рюкзак
+                _slot[i + 19].SetPosition(PosX + 256 + i * 50, PosY + 45);
+                _slot[i + 24].SetPosition(PosX + 256 + i * 50, PosY + 95);
+                _slot[i + 29].SetPosition(PosX + 256 + i * 50, PosY + 145);
+                _slot[i + 34].SetPosition(PosX + 256 + i * 50, PosY + 195);
+                _slot[i + 39].SetPosition(PosX + 256 + i * 50, PosY + 245);
+            }
+        }
+
+        /// <summary>
+        /// Клик за пределами окна
+        /// </summary>
+        protected override void _OnClickOutsideWindow() => _ThrowTheSlot();
+
+        /// <summary>
+        /// Выбросить слот который в руке
+        /// </summary>
+        protected virtual void _ThrowTheSlot()
+        {
+            if (_stakAir != null)
+            {
+                _windowMvk.Game.TrancivePacket(
+                    new PacketC0EClickWindow((byte)EnumActionClickWindow.ThrowOutAir));
+            }
+        }
+
+        /// <summary>
+        /// Происходит перед закрытием окна
+        /// </summary>
+        protected virtual void _OnFinishing() => _windowMvk.Game.TrancivePacket(
+            new PacketC0EClickWindow((byte)EnumActionClickWindow.Close));
+        
+        public override void Dispose()
+        {
+            base.Dispose();
+            _OnFinishing();
+        }
+    }
+}
